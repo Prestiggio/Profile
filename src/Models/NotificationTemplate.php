57ea76bf997beed35f2d53d7cbb5cc96ecf4ee1f
Landr Rakotoarivelo <folojona@gmail.com>
2019-01-28 18:41:07 +0200

    mail management

--- src/Models/NotificationTemplate.php
+++ src/Models/NotificationTemplate.php
@@ -4,10 +4,19 @@ namespace Ry\Profile\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Ry\Medias\Models\Traits\MediableTrait;
+use Ry\Admin\Listeners\MailSender;
+use Ry\Admin\Listeners\SmsSender;
+use Ry\Admin\Listeners\MessengerSender;
 
 class NotificationTemplate extends Model
 {
     use MediableTrait;
+    
+    const CHANNELS = [
+        MailSender::class => 'e_mail',
+        SmsSender::class => 'sms',
+        MessengerSender::class => 'messenger'
+    ];
     
     protected $table = "ry_profile_notification_templates";
     
