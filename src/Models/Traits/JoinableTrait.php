eaa0806d0cfcff1b905123829bb0f499b3fa940f
Landr Rakotoarivelo <folojona@gmail.com>
2019-03-27 19:30:56 +0200

    ncontacts dump

--- src/Models/Traits/JoinableTrait.php
+++ src/Models/Traits/JoinableTrait.php
@@ -3,8 +3,23 @@ namespace Ry\Profile\Models\Traits;
 
 trait JoinableTrait
 {
+    private $cacheContacts;
+    
 	public function contacts() {
 		return $this->morphMany("Ry\Profile\Models\Contact", "joinable");
+	}
+	
+	public function getNcontactsAttribute() {
+	    if(!isset($this->cacheContacts)) {
+	        $this->cacheContacts = $this->contacts;
+	    }
+	    $ar = [];
+	    if($this->cacheContacts) {
+	        foreach($this->cacheContacts as $contact) {
+	            $ar[$contact->ndetail['type']][$contact->ndetail['schedule']] = $contact->ndetail['value'];
+	        }
+	    }
+	    return $ar;
 	}
 	
 	public function getCompleteContactsAttribute() {
