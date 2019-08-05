e32583c6a2e9cb7cff72aa5c97591912f39a0e00
Landr Rakotoarivelo <folojona@gmail.com>
2019-08-05 21:14:41 +0200

    notifications emailings

--- src/Models/NotificationTemplate.php
+++ src/Models/NotificationTemplate.php
@@ -7,6 +7,7 @@ use Ry\Medias\Models\Traits\MediableTrait;
 use Ry\Admin\Listeners\MailSender;
 use Ry\Admin\Listeners\SmsSender;
 use Ry\Admin\Listeners\MessengerSender;
+use Ry\Admin\Models\Alert;
 
 class NotificationTemplate extends Model
 {
@@ -14,33 +15,15 @@ class NotificationTemplate extends Model
     
     const CHANNELS = [
         MailSender::class => 'e_mail',
-        SmsSender::class => 'sms',
-        MessengerSender::class => 'messenger'
+        /*SmsSender::class => 'sms',
+        MessengerSender::class => 'messenger'*/
     ];
     
     protected $table = "ry_profile_notification_templates";
     
     protected $with = ['medias'];
     
-    protected $appends = ["archannels", "arevents", "arinjections"];
-    
-    protected $visible = ["id", "name", "archannels", "arevents", "arinjections", "medias"];
-    
-    public function getArinjectionsAttribute() {
-        return json_decode($this->injections, true);
-    }
-    
-    public function setArinjectionsAttribute($ar) {
-        $this->injections = json_encode($ar);
-    }
-    
-    public function getAreventsAttribute() {
-        return json_decode($this->events, true);
-    }
-    
-    public function setAreventsAttribute($ar) {
-        $this->events = json_encode($ar);
-    }
+    protected $appends = ["archannels"];
     
     public function getArchannelsAttribute() {
         return json_decode($this->channels, true);
@@ -48,5 +31,9 @@ class NotificationTemplate extends Model
     
     public function setArchannelsAttribute($ar) {
         $this->channels = json_encode($ar);
+    }
+    
+    public function alerts() {
+        return $this->belongsToMany(Alert::class, 'ry_profile_template_alerts', 'template_id', 'alert_id');
     }
 }
---
+++ src/Models/TemplateAlert.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ry\Profile\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TemplateAlert extends Model
+{
+    protected $table = "ry_profile_template_alerts";
+}
